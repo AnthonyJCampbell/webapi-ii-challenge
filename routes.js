142a020989232ee5ef01210abd01aa1d404b6f91
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-19 14:17:00 +0100

    Reimplemented .get to use async, rather than

--- routes.js
+++ routes.js
@@ -8,15 +8,24 @@ routes.post('/', (req, res) => {
 
 })
 
-routes.get('/api/posts', (req, res) => {
-  Posts.find(req.query)
-    .then(data => {
-      res.status(200).json(data)
-    })
-    .catch(err => {
-      res.status(500).json({ error: "The posts information could not be retrieved." })
-    });
+routes.get('/api/posts', async (req, res) => {
+  try {
+    const data = await Posts.find(req.query);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "The posts information could not be retrieved." })
+  }
 })
+
+// routes.get('/api/posts', (req, res) => {
+//   Posts.find(req.query)
+//     .then(data => {
+//       res.status(200).json(data)
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: "The posts information could not be retrieved." })
+//     });
+// })
 
 routes.get('/:id', (req, res) => {
   
