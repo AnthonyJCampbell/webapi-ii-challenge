4ba7f9e464766d0c791ffda32b4c872a93800df7
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-19 14:11:45 +0100

    Bug Fix. Initialized Project and README.md

---
+++ routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const routes = express.Router();
+const Posts = require('./data/db');
+
+routes.use(express.json());
+
+routes.post('/', (req, res) => {
+
+})
+
+routes.get('/api/posts', (req, res) => {
+  Posts.find(req.query)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+})
+
+routes.get('/:id', (req, res) => {
+    
+})
+
+routes.delete('/:id', (req, res) => {
+    
+})
+
+routes.put('/:id', (req, res) => {
+    
+})
+
+module.exports = routes;
