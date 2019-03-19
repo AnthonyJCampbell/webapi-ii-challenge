21236ad12e036cf9f500c376c00b1e001bd6e1ff
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-19 14:43:58 +0100

    Added and tested .get by ID method

--- routes.js
+++ routes.js
@@ -44,15 +44,31 @@ routes.get('/api/posts', async (req, res) => {
 //     });
 // })
 
-routes.get('api/posts/:id', (req, res) => {
-  
+routes.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  Posts.findById(id)
+    .then(data => {
+      console.log(req.params)
+      if (!data) {
+        res.status(404).json({ 
+          message: "The post with the specified ID does not exist." 
+        })
+      } else {
+        res.status(200).json(data);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      })
+    })
 })
 
-routes.delete('api/posts/:id', (req, res) => {
+routes.delete('/api/posts/:id', (req, res) => {
 
 })
 
-routes.put('api/posts/:id', (req, res) => {
+routes.put('/api/posts/:id', (req, res) => {
   
 })
 
