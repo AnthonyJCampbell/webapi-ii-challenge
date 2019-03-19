d067fc1bf91e7e63d9aa725a75bc86a5d0c16a6c
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-19 14:36:27 +0100

    Added Post functionality using .then methodology

--- routes.js
+++ routes.js
@@ -4,16 +4,33 @@ const Posts = require('./data/db');
 
 routes.use(express.json());
 
-routes.post('/', (req, res) => {
-
-})
+routes.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({ 
+      errorMessage: "Please provide title and contents for the post."
+    })
+  } else {
+    Posts.insert(req.body)
+      .then(data => {
+        res.status(201).json({ ...data, title, contents });
+      })
+      .catch(() =>
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        })
+      );
+  }
+});
 
 routes.get('/api/posts', async (req, res) => {
   try {
     const data = await Posts.find(req.query);
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "The posts information could not be retrieved." })
+    res.status(500).json({ 
+      error: "The posts information could not be retrieved." 
+    })
   }
 })
 
@@ -27,15 +44,15 @@ routes.get('/api/posts', async (req, res) => {
 //     });
 // })
 
-routes.get('/:id', (req, res) => {
+routes.get('api/posts/:id', (req, res) => {
   
 })
 
-routes.delete('/:id', (req, res) => {
+routes.delete('api/posts/:id', (req, res) => {
 
 })
 
-routes.put('/:id', (req, res) => {
+routes.put('api/posts/:id', (req, res) => {
   
 })
 
